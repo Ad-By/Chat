1a8a6d9606ee3fd1d59caa700a3c2377fcca4e55
Adam Byrne <adambyrney@outlook.com>
2017-06-05 20:56:32 +0100

    commit 1

---
+++ Chat/ViewModel/WindowViewModel.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Chat
+{
+    /// <summary>
+    /// The View Model for the custom window
+    /// </summary>
+    public class WindowViewModel : BaseViewModel
+    {
+        #region Fields
+
+        private Window _window;
+        private int _outerMarginSize = 10;
+        private int _windowRadius = 10;
+
+        #endregion
+
+        #region Properties
+
+        public int ResizeBorder { get; set; } = 6;
+
+        public Thickness ResizeBorderThickness { get { return new Thickness(ResizeBorder + OuterMarginSize); } }
+
+        public int OuterMarginSize
+        {
+            get
+            {
+                return _window.WindowState == WindowState.Maximized ? 0 : _outerMarginSize;
+            }
+            set
+            {
+                _outerMarginSize = value;
+            }
+        }
+
+        public Thickness OuterMarginSizeThickness { get { return new Thickness(OuterMarginSize); } }
+
+        public int WindowRadius
+        {
+            get
+            {
+                return _window.WindowState == WindowState.Maximized ? 0 : _windowRadius;
+            }
+            set
+            {
+                _windowRadius = value;
+            }
+        }
+
+        public CornerRadius WindowCornerRadius { get { return new CornerRadius(WindowRadius); } }
+
+        public int TitleHeight { get; set; } = 42;
+
+        public GridLength TitleHeightGridLength { get { return new GridLength(TitleHeight); } }
+
+        #endregion
+
+        #region Constructor
+
+        public WindowViewModel(Window window)
+        {
+            _window = window;
+
+            //Listen out for the window resizing
+            window.StateChanged += (sender, e) => 
+            {
+                OnPropertyChanged(nameof(ResizeBorderThickness));
+                OnPropertyChanged(nameof(OuterMarginSize));
+                OnPropertyChanged(nameof(OuterMarginSizeThickness));
+                OnPropertyChanged(nameof(WindowRadius));
+                OnPropertyChanged(nameof(WindowCornerRadius));
+            };
+        }
+
+        #endregion
+
+    }
+}
